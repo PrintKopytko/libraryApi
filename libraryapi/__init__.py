849f1076c7034e14050aa147ff3e5d05a7b0157b
Patryk Kowalczyk <patryk8885@gmail.com>
2024-06-24 10:27:51 +0200

    add swagger and make app wait for db

--- libraryapi/__init__.py
+++ libraryapi/__init__.py
@@ -1,5 +1,10 @@
+import logging
+import time
+
+from flasgger import Swagger
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 from .config import Config
 
 db = SQLAlchemy()
@@ -7,14 +12,39 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.logger.setLevel(logging.INFO)
+    swagger = Swagger(app, template={
+        "info": {
+            "title": "My API",
+            "description": "API for demonstrating Swagger with Flask",
+            "version": "1.0.0"
+        },
+        "host": "localhost:8000",
+        "basePath": "/api",
+        "schemes": ["http"],
+    })
     app.config.from_object(Config)
 
     db.init_app(app)
 
     with app.app_context():
         from .routes import books_bp
-        app.register_blueprint(books_bp, url_prefix='/books')
+        app.register_blueprint(books_bp, url_prefix='/api/books')
 
-        db.create_all()
-
+        tries = 10
+        attempt = 1
+        interval = 2
+        app.logger.info("Creating the database...")
+        while attempt < tries:
+            try:
+                db.create_all()
+            except Exception as ex:
+                attempt += 1
+                app.logger.warning(f"Failed to create db, attempt {attempt}/{tries} in {interval}s")
+                if attempt > tries:
+                    raise ex
+                time.sleep(interval)
+            else:
+                app.logger.info("Created the database successfully!")
+                break
     return app
--- libraryapi/routes.py
+++ libraryapi/routes.py
@@ -7,5 +7,12 @@ books_bp = Blueprint('books', __name__)
 
 @books_bp.route('/', methods=['GET'])
 def get_books():
+    """
+    Get all books
+    ---
+    responses:
+      200:
+        description: A successful response
+    """
     books = Book.query.all()
     return jsonify([book.to_dict() for book in books])
